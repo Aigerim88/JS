95c3655da994bec5ccf2c203f8dafa00c68e1a8a
Aigerim <75934961+Aigerim88@users.noreply.github.com>
2021-12-10 17:12:52 +0600

    added srting

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let a = Number(prompt("Введите высоту вашей стены:"));
 let b = Number(prompt("Введите ширину вашей стены:"));
 let s = a * b;
-console.log("Вам необходимо " + s + " кв.м. обоев");
+console.log("Вам необходимо " + String(s) + " кв.м. обоев");

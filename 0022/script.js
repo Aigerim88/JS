c619d5ce6c74a8a255091aac037a4c4c57708299
Aigerim <75934961+Aigerim88@users.noreply.github.com>
2021-12-24 16:29:45 +0600

    Homework #0022 is done

---
+++ 0022/script.js
@@ -0,0 +1,13 @@
+let myArray1 = [];
+let myArray2 = [];
+
+for (i = 1; i < 101; i++) {
+    if (i % 3 === 0) {
+        myArray1.push(i);
+    } else if (i % 4 === 0){
+        myArray2.push(i);
+    }
+}
+
+console.log(myArray1);
+console.log(myArray2);

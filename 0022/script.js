fc31cddaaaffaa007cde3d3d589f11dbfd92934c
Aigerim <75934961+Aigerim88@users.noreply.github.com>
2021-12-24 17:58:41 +0600

    added let

--- 0022/script.js
+++ 0022/script.js
@@ -1,7 +1,7 @@
 let myArray1 = [];
 let myArray2 = [];
 
-for (i = 1; i < 101; i++) {
+for (let i = 1; i < 101; i++) {
     if (i % 3 === 0) {
         myArray1.push(i);
     } else if (i % 4 === 0){

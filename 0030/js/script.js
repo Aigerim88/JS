59e1299f3f1fe34393bcafee31ff850437372b70
Aigerim <75934961+Aigerim88@users.noreply.github.com>
2022-01-29 16:37:55 +0600

    Calculator is working properly now

--- 0030/js/script.js
+++ 0030/js/script.js
@@ -1,58 +1,58 @@
-document.addEventListener("DOMContentLoaded", run)
+document.addEventListener("DOMContentLoaded", () => {
+    const inputElem = document.getElementById("calc__screen");
+    const one = document.getElementById("calc__1");
+    const two = document.getElementById("calc__2");
+    const three = document.getElementById("calc__3");
+    const four = document.getElementById("calc__4");
+    const five = document.getElementById("calc__5");
+    const six = document.getElementById("calc__6");
+    const seven = document.getElementById("calc__7");
+    const eight = document.getElementById("calc__8");
+    const nine = document.getElementById("calc__9");
+    const zero = document.getElementById("calc__0");
+    const plus = document.getElementById("calc__plus");
+    const minus = document.getElementById("calc__minus");
+    const division = document.getElementById("calc__div");
+    const mult = document.getElementById("calc__mult");
+    const root = document.getElementById("calc__root");
+    const expon = document.getElementById("calc__exp");
+    const equal = document.getElementById("calc__equal");
+    const c = document.getElementById("calc__c");
 
-function run() {
-    let a = ""; //первое число
-    let b = ""; // второе число
-    let sign = ""; // знак операции
-    let finish = false;
-
-    const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    const action = ["-", "+", "/", "*", "^", "√"];
-
-    // экран
-    const out = document.querySelector(".calc-screen p");
-
-    // функция очистки - кнопка С
-    function clearAll () {
-        let a = ""; 
-        let b = ""; 
-        let sign = ""; 
-        let finish = false;
-        out.textContent = 0;
+    let myBtn = [one, two, three, four, five, six, seven, eight, nine, zero, plus, minus, division, mult, root, expon];
+    let myValue = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "/", "*", "√", "^"];
+    for (let i = 0; i < myBtn.length && i < myValue.length; i++) {
+        myBtn[i].addEventListener("click", () => {
+                inputElem.value += myValue[i];
+            })
     }
 
-    // добавляем событие на кнопку очистки С
-    document.querySelector(".c").addEventListener("click", clearAll);
-
-    // добавляю событие на кнопки
-    const key = document.querySelector(".calc__item");
-    key.addEventListener("click", btn);
-    
-    function btn() {
-        // если нажата цифровая кнопка
-       if(digit.includes(key.value)) {
-            if (b === "" && sign === "") {
-                a += key.value;
-                console.log( a, b, sign);
-                out.textContent = a;
-            } else if (a !== "" && b !== "" && finish) {
-
-            } else {
-                b += key.value;
-                out.textContent = b;
-            }
-            // если нажата кнопка операции
-            if(action.includes(key.value)) {
-                sign = key.value;
-                out.textContent = sign;
-                console.log( a, b, sign);
-                return
-            }
-
-            //нажата кнопка =
-            if (key.value === "=") {
-                
+    equal.addEventListener("click", calculate)
+    function calculate () {
+        let si = 0;
+        for( let i = 0; i < inputElem.value.length; i++) {
+            if ("+-/*√^".includes(inputElem.value[i])) {
+                si = i;
             }
         }
-    }   
-}
+        const firstNum = Number(inputElem.value.slice(0, si));
+        const secondNum = Number(inputElem.value.slice(si + 1, inputElem.value.length));
+        let result = 0;
+        if (inputElem.value[si] == "+") result = firstNum + secondNum;
+        else if (inputElem.value[si] == "-") result = firstNum - secondNum;
+        else if (inputElem.value[si] == "*") result = firstNum * secondNum;
+        else if (inputElem.value[si] == "/") result = firstNum / secondNum;
+        else if (inputElem.value[si] == "^") result = Math.pow(firstNum, secondNum);
+        else if (inputElem.value[si] == "√") {
+            let res = Number(inputElem.value.slice(1, inputElem.value.length));
+            result = Math.sqrt(res);
+        }
+        inputElem.value = result;
+    }
+
+    c.addEventListener("click", clearAll);   
+
+    function clearAll() {
+        inputElem.value = "";
+    }
+})

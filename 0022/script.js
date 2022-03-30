e90f612e1c992e92f9b4aedb4f6c6798e232d6e1
Aigerim Erkebaeva <erkebaeva.aigerim88@gmail.com>
2022-03-30 13:30:17 +0600

    correct else if to if

--- 0022/script.js
+++ 0022/script.js
@@ -4,7 +4,8 @@ let myArray2 = [];
 for (let i = 1; i < 101; i++) {
     if (i % 3 === 0) {
         myArray1.push(i);
-    } else if (i % 4 === 0){
+    }
+    if (i % 4 === 0){
         myArray2.push(i);
     }
 }
